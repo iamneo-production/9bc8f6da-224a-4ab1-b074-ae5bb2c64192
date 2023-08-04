8d73f1a87803ddeabe0a448bd5881ab164b221bb
Srinath171 <harisrinath88@gmail.com>
2023-08-04 12:23:52 +0000

    payment-service linked to service-registry

--- payment-service/src/main/java/com/microservices/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/microservices/paymentservice/PaymentServiceApplication.java
@@ -2,12 +2,22 @@ package com.microservices.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentServiceApplication.class, args);
+	}
+
+	@RequestMapping(value = "/")
+	public String message(){
+		return "test..";
 	}
 
 }
